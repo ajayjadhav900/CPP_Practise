2c8b7e8dd95985258405dd0bbd352113d56b418e
ajayjadhav900 <ajay.jadhav900@gmail.com>
2023-05-20 18:42:42 +0530

    added logger

--- design_Patterns/Adapter.cpp
+++ design_Patterns/Adapter.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 #include "Adapter.hpp"
+#include "logger.hpp"
 using namespace std;
+void logTest(void);
+loglevel_e loglevel = logDEBUG2; // Define the log level variable
+
 void English::write()
 {
-    cout << " I am English, Standard language logs.\n";
+    log(logDEBUG1) <<  " I am English, Standard language logs.\n";
 }
 
 void English::standardTimings()
 {
-    cout << "convert to standard time zone\n";
+    log(logDEBUG1) <<  "convert to standard time zone\n";
 }
 
 void German::write()
 {
-    cout << "Uses German langugae for the logs.\n";
+    log(logDEBUG1) << "Uses German langugae for the logs.\n";
 }
 
 void German::standardTimings()
 {
-    cout << "Converted to German timings\n";
+    log(logDEBUG2) <<  "Converted to German timings\n";
 }
 
 void AdapterLangConverter::write()
@@ -38,6 +42,9 @@ void LogsConverter(ILanguage *lang)
 
 int main()
 {
+    //loglevel_e loglevel;
+
+   loglevel = logDEBUG2;
     ILanguage *eng = new English();
     eng->write();
     eng->standardTimings();
@@ -46,8 +53,28 @@ int main()
     ger->write();
     ger->standardTimings();
 
-    cout << "want the english language from German";
+    log(logDEBUG1) <<  "want the english language from German";
     AdapterLangConverter *alc = new AdapterLangConverter(ger);
     LogsConverter(alc);
+
+    //logTest();
     return 0;
+}
+
+void logTest(void) {
+    loglevel_e loglevel_save = loglevel;
+
+    loglevel_save = logDEBUG1;
+
+    log(logINFO) << "foo " << "bar " << "baz";
+
+    int count = 3;
+    log(logDEBUG) << "A loop with "    << count << " iterations";
+    for (int i = 0; i != count; ++i)
+    {
+        log(logDEBUG1) << "the counter i = " << i;
+        log(logDEBUG2) << "the counter i = " << i;
+    }
+
+    loglevel = loglevel_save;
 }
